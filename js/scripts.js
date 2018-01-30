86e256bc2f0bbdc4674e5e362a4fa300dc26e7ef
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-31 00:59:22 +0100

    Add logic to convert a complex binary number to decimal

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-var binaryConverter = function(number) {
-  return number;
+var binaryConverter = function(stringArray) {
+  var numbers = stringArray.map(function(number) {
+    return parseInt(number);
+  });
+  numbers.reverse();
+
+  for (var i = 0; i < numbers.length ; i++) {
+    numbers[i] *= Math.pow(2, i);
+  }
+
+  var convertedNumber = numbers.reduce(function(first, number) {
+    return first + number;
+  });
+
+  return convertedNumber;
 };
 
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var number = $("#numbers").val();
+    var number = $("#numbers").val().toString().split("");
     var result = binaryConverter(number);
     $("#result").text(result);
   });
