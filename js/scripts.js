45ddd4a94db46509d0db4afe9925c47a7c8bcfce
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-31 10:37:01 +0100

    Create a function called 'convertAndReverse' that converts the user input from string to integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-var binaryConverter = function(stringArray) {
-  var numbers = stringArray.map(function(number) {
+var convertAndReverse = function(stringArray) {    //to convert every string in the array to number
+  var numbers = stringArray.map(function(number) { //and reverse it
     return parseInt(number);
   });
-  numbers.reverse();
+  return numbers.reverse();
+};
+
+/*var ternaryConverter = function(stringArray) {
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i] *= Math.pow(3, i);
+  }
+};*/
+
+var binaryConverter = function(stringArray) {
+  var numbers = convertAndReverse(stringArray);
 
   for (var i = 0; i < numbers.length ; i++) {
     numbers[i] *= Math.pow(2, i);
@@ -19,7 +29,7 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var number = $("#numbers").val().toString().split("");
+    var number = $("#numbers").val().split("");
     var result = binaryConverter(number);
     $("#result").text(result);
   });
